71915546fea1983b3f4f75e0201f8e87b759563d
Mikhail Kocherov <ppchaos@mail.ru>
2026-05-12 08:39:16 +0300

    17_rl. 17c_ppo_trxl_memory_01.ipynb achieved the same performance as reference implementation, launch id: 17c_ppo_trxl_memory_01/5. Main culprit was in optimizer - should use AdamW instead of Adam

--- lib/math_utils.py
+++ lib/math_utils.py
@@ -1,4 +1,7 @@
+from collections import deque
 import numpy as np
+
+import lang_utils as lu
 
 def softmax(x):
     max_x = np.max(x)
@@ -43,4 +46,28 @@ class RecursiveAverageFilter:
 
     def __repr__(self):
         return f'RecursiveAverageFilter(v={self.v}, n={self.n})'
+
+class RecursiveMovingAverageFilter:
+    def __init__(self, max_n):
+        self.v = 0
+        self.max_n = max_n
+        self.elems = deque(maxlen=max_n)
+
+    def __call__(self, x):
+        if not self.elems:
+            self.v = x
+        else:
+            first_elem = lu.when(len(self.elems) == self.max_n, lambda: self.elems[0], None)
+            # Logic is based on idea: AVG(elems) * LEN(elems) = SUM(elems)
+            window_sum = self.v * len(self.elems) - lu.coalesce(first_elem, 0) + x
+            self.v = window_sum / min(len(self.elems) + 1, self.max_n)
+            
+        self.elems.append(x)
+        return self.v
+
+    def __str__(self):
+        return str(self.v)
+
+    def __repr__(self):
+        return f'RecursiveMovingAverageFilter(v={self.v}, n={len(self.elems)}, max_n={self.max_n})'
     
