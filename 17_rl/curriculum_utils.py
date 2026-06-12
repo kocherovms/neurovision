674100b1253b70b48ef31512d5a3496585bd2c38
Mikhail Kocherov <ppchaos@mail.ru>
2026-06-12 16:38:47 +0300

    17_rl. 17e_ppo_tr_atari_mp_10.ipynb: move to action plan prediction instead of a single action

--- 17_rl/curriculum_utils.py
+++ 17_rl/curriculum_utils.py
@@ -2,15 +2,16 @@ import numpy as np
 import scipy.stats
 
 class SmoothBoxEnergy:
-    def __init__(self, lo, hi, scale):
+    def __init__(self, lo, hi, scale, min_right=0):
         self.lo = lo
         self.hi = hi
         self.scale = scale
         self.norm_coef = scipy.stats.halfcauchy.pdf(0, scale=scale)
+        self.min_right = min_right
 
     # energy density function (analog to PDF but unlike PDF integral over EDF is not one)
     def edf(self, x):
-        return np.where(
+        y = np.where(
             x < self.lo,
             scipy.stats.halfcauchy.pdf(self.lo - x, scale=self.scale) / self.norm_coef,
             np.where(
@@ -18,4 +19,9 @@ class SmoothBoxEnergy:
                 scipy.stats.halfcauchy.pdf(x, loc=self.hi, scale=self.scale) / self.norm_coef,
                 1,
             )
-        )    
+        )
+
+        if self.min_right > 0:
+            y = np.where(x > self.hi, np.where(y > self.min_right, y, self.min_right), y)
+
+        return y
