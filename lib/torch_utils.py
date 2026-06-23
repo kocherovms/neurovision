afe3554eeb679479a5f3c9afe48a0412ec497a24
Mikhail Kocherov <ppchaos@mail.ru>
2026-06-23 11:49:44 +0300

    lib. Consolidate params annealers

--- lib/torch_utils.py
+++ lib/torch_utils.py
@@ -55,16 +55,6 @@ def train_guard(model):
 def numpy_dtype_to_torch_dtype(np_dtype):
     return torch.from_numpy(np.array([], dtype=np_dtype)).dtype
 
-def get_linear_anneal(start_value, end_value, steps_count):
-    def anneal_param(step):
-        if steps_count <= 0 or step >= steps_count:
-            return end_value
-            
-        frac = 1 - step / steps_count
-        return (start_value - end_value) * frac + end_value
-
-    return anneal_param
-
 def get_grad_norm_groups(model, group_names):
     norms = defaultdict(list)
     
@@ -83,3 +73,69 @@ def get_grad_norm_groups(model, group_names):
         result[k] = math.sqrt(sum(v)) if v else 0.0
 
     return result
+    
+def get_old_linear_anneal(start_value, end_value, steps_count):
+    def anneal_param(step):
+        if steps_count <= 0 or step >= steps_count:
+            return end_value
+            
+        frac = 1 - step / steps_count
+        return (start_value - end_value) * frac + end_value
+
+    return anneal_param
+
+def get_anneal(anneal_name, *args, **kwargs):
+    match anneal_name:
+        case 'const': return get_const_anneal(*args, **kwargs)
+        case 'linear': return get_linear_anneal(*args, **kwargs)
+        case 'trapezoid': return get_trapezoid_anneal(*args, **kwargs)
+        case 'shark_fin': return get_shark_fin_anneal(*args, **kwargs)
+        case _: assert False, f'Unsupported {anneal_name=}'
+
+def get_const_anneal(a):
+    def thunk(t):
+        return a
+
+    return thunk
+
+def get_linear_anneal(a, b):
+    def thunk(t):
+        assert 0 <= t <= 1, t
+        return a + t * (b - a)
+
+    return thunk
+
+def get_trapezoid_anneal(a, b, c, a_dur, b_dur):
+    assert a_dur >= 0, a_dur
+    assert b_dur >= 0, b_dur
+    assert 0 <= (a_dur + b_dur) <= 1, (a_dur, b_dur)
+    c_dur = 1 - a_dur - b_dur
+    
+    def thunk(t):
+        if t < a_dur and a_dur > 0:
+            return a + t * (b - a) / a_dur
+        elif a_dur <= t < (a_dur + b_dur):
+            return b
+        else:
+            if c_dur > 0:
+                t = t - a_dur - b_dur
+                return b + t * (c - b) / c_dur
+            else:
+                return c
+
+    return thunk
+
+def get_shark_fin_anneal(a, b, warm_dur=0.05):
+    assert 0 <= warm_dur <= 1, warm_dur
+    anneal_dur = 1 - warm_dur
+    assert anneal_dur >= 0, anneal_dur
+    
+    def thunk(t):
+        if t < warm_dur and warm_dur > 0:
+            return 0 + t * (a - 0) / warm_dur
+            
+        progress = (t - warm_dur) / anneal_dur
+        c = 0.5 * (1.0 + math.cos(math.pi * progress)) # smooth cosine from 1 to 0
+        return a + (b - a) * (1 - c)
+
+    return thunk
