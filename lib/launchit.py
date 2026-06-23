281e538dc67159c7248e0db1445073ea7a1afd8e
Mikhail Kocherov <ppchaos@mail.ru>
2026-06-23 13:34:53 +0300

    lib. launchit: make it possible to hack (e.g. collect source code) notebook without creating any files (in memory)

--- lib/launchit.py
+++ lib/launchit.py
@@ -25,31 +25,18 @@ class ExecGraphEntry:
     stop_source_line_ind: int = None
     index: str = None
 
-def launchit(fname, launch_serial=0, expandvars={}, make_py_file=False, dir_name='', max_serials_count=1_000, collect_inds=None, disable_inds=None):
-    fname_dir = os.path.dirname(fname) if not dir_name else dir_name
-    fname_name = os.path.splitext(os.path.basename(fname))[0]
-    fname_ext = os.path.splitext(fname)[1] if not make_py_file else '.py'
-    
-    new_fname = ''
-    serials_range = range(1, max_serials_count + 1) if launch_serial == 0 else [launch_serial]
+class NotebookProcessor:
+    def __init__(self):
+        self.nb = None
+        self.exec_graph = None
+        self.collected_source_lines = None
 
-    for i in serials_range:
-        new_fname = os.path.join(fname_dir, f'{fname_name}-launch{i}{fname_ext}')
-    
-        if not os.path.exists(new_fname):
-            break
-    else:
-        raise Exception(f'Failed to generate new launch file name: all variants are taken')
-
-    Logging.debug(f'Creating {new_fname}')
-    
-    with open(fname, 'r') as f:
-        nb = json.load(f)
-        exec_graph = []
-        collected_source_lines = defaultdict(list) # collect index -> source lines
-    
-        for cell_ind, cell in enumerate(nb['cells']):
-            
+    def __call__(self, f, new_fname, expandvars, collect_inds, disable_inds):
+        self.nb = json.load(f)
+        self.exec_graph = []
+        self.collected_source_lines = defaultdict(list) # collect index -> source lines
+        
+        for cell_ind, cell in enumerate(self.nb['cells']):
             for source_line_ind, source_line in enumerate(cell['source']):
                 source_line = source_line.strip()
                 m = re.match(r'^(.*)#\s*@launchit\.(\w+)\s*$', source_line)
@@ -80,21 +67,21 @@ def launchit(fname, launch_serial=0, expandvars={}, make_py_file=False, dir_name
                             assert not ege.is_oneliner, '@launchit.stop cannot be oneliner'
                             assert index is None, f'@launchit.stop does not support indexing, {command_with_index=}'
                             # look behind and patch stop_source_line_ind
-                            for lb_ege_ind in range(len(exec_graph) - 1, -1, -1):
-                                lb_ege = exec_graph[lb_ege_ind]
+                            for lb_ege_ind in range(len(self.exec_graph) - 1, -1, -1):
+                                lb_ege = self.exec_graph[lb_ege_ind]
                                 
                                 if lb_ege.cell_ind != cell_ind:
                                     raise Exception(f'Cell {cell_ind}, line {source_line_ind}, @launchit.stop has no preceeding command')
                                 elif lb_ege.stop_source_line_ind == -1:
-                                    exec_graph[lb_ege_ind].stop_source_line_ind = source_line_ind
+                                    self.exec_graph[lb_ege_ind].stop_source_line_ind = source_line_ind
                                     Logging.trace(f'Cell {cell_ind}, command {lb_ege.command.name} at line {lb_ege.source_line_ind} will stop at line {source_line_ind}')
                                     break
 
                         if ege.command != Command.STOP:
-                            exec_graph.append(ege)
+                            self.exec_graph.append(ege)
 
-        for ege in sorted(exec_graph):
-            cell = nb['cells'][ege.cell_ind]
+        for ege in sorted(self.exec_graph):
+            cell =self.nb['cells'][ege.cell_ind]
             stop_source_line_ind = len(cell['source']) if ege.stop_source_line_ind == -1 else ege.stop_source_line_ind
             
             match ege.command:
@@ -118,20 +105,20 @@ def launchit(fname, launch_serial=0, expandvars={}, make_py_file=False, dir_name
                         if ege.is_oneliner:
                             Logging.trace(f'Cell {ege.cell_ind}, collecting source line {ege.source_line_ind}, index={ege.index}')
                             source_line = cell['source'][ege.source_line_ind]
-                            collected_source_lines[ege.index].append(source_line)
+                            self.collected_source_lines[ege.index].append(source_line)
                         else:
                             assert ege.source_line_ind + 1 < stop_source_line_ind
                             Logging.trace(f'Cell {ege.cell_ind}, collecting source lines from {ege.source_line_ind + 1} to {stop_source_line_ind}, index={ege.index}')
         
-                            if collected_source_lines:
-                                collected_source_lines[ege.index].append('\n')
+                            if self.collected_source_lines:
+                                self.collected_source_lines[ege.index].append('\n')
             
                             for source_line_ind in range(ege.source_line_ind + 1, stop_source_line_ind):
                                 source_line = cell['source'][source_line_ind]
-                                collected_source_lines[ege.index].append(source_line)
+                                self.collected_source_lines[ege.index].append(source_line)
                 
                 case Command.COLLECTED:
-                    my_collected_source_lines = collected_source_lines.get(ege.index, [])
+                    my_collected_source_lines = self.collected_source_lines.get(ege.index, [])
                     my_collected_source_lines.append('')
                     
                     Logging.trace(f'Cell {ege.cell_ind} (len={len(cell['source'])}), ' + 
@@ -145,7 +132,7 @@ def launchit(fname, launch_serial=0, expandvars={}, make_py_file=False, dir_name
 
                     successors = filter(
                         lambda x: x.command == Command.COLLECTED and x.cell_ind == ege.cell_ind and x.source_line_ind > ege.source_line_ind,
-                        exec_graph,
+                        self.exec_graph,
                     )
                     
                     for successor in successors:
@@ -185,18 +172,42 @@ def launchit(fname, launch_serial=0, expandvars={}, make_py_file=False, dir_name
         expandvars['LAUNCHIT_FNAME'] = new_fname
         Logging.trace(f'{expandvars=}')
         
-        for cell in nb['cells']:
+        for cell in self.nb['cells']:
             for source_line_ind, source_line in enumerate(cell['source']):
                 t = string.Template(source_line)
                 cell['source'][source_line_ind] = t.safe_substitute(expandvars)
+
+    
+def launchit(fname, launch_serial=0, expandvars={}, make_py_file=False, dir_name='', max_serials_count=1_000, collect_inds=None, disable_inds=None):
+    fname_dir = os.path.dirname(fname) if not dir_name else dir_name
+    fname_name = os.path.splitext(os.path.basename(fname))[0]
+    fname_ext = os.path.splitext(fname)[1] if not make_py_file else '.py'
+    
+    new_fname = ''
+    serials_range = range(1, max_serials_count + 1) if launch_serial == 0 else [launch_serial]
+
+    for i in serials_range:
+        new_fname = os.path.join(fname_dir, f'{fname_name}-launch{i}{fname_ext}')
+    
+        if not os.path.exists(new_fname):
+            break
+    else:
+        raise Exception(f'Failed to generate new launch file name: all variants are taken')
+
+    Logging.debug(f'Creating {new_fname}')
+    
+    processor = NotebookProcessor()
+    
+    with open(fname, 'r') as f:
+        processor(f, new_fname=new_fname, expandvars=expandvars, collect_inds=collect_inds, disable_inds=disable_inds)
     
     with open(new_fname, 'w') as f:
         if make_py_file:
-            for cell in nb['cells']:
+            for cell in processor.nb['cells']:
                 if cell['cell_type'] == 'code':
                     f.writelines(cell['source'])
                     f.write('\n\n')
         else:
-            json.dump(nb, f, indent=2) # .ipynb
+            json.dump(processor.nb, f, indent=2) # .ipynb
     
     return new_fname
