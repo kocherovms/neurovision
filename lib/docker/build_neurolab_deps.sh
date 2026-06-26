bac30390094cd13c44be0fefa120afa77de6dc29
Mikhail Kocherov <ppchaos@mail.ru>
2026-06-26 22:02:42 +0300

    lib. Keep docker push only for neurolab_source

--- lib/docker/build_neurolab_deps.sh
+++ lib/docker/build_neurolab_deps.sh
@@ -4,5 +4,3 @@ set -x
 
 # Use cachebust technique to force ale_py reinstall
 docker build -f neurolab_deps.Dockerfile   -t cr.selcloud.ru/neurolab/neurolab_deps:latest   -t neurolab_deps:latest --add-host nexus=127.0.0.1 --network host --build-arg CACHEBUST=$(date +%s) . 
-docker build -f neurolab_source.Dockerfile -t cr.selcloud.ru/neurolab/neurolab_source:latest -t neurolab_source:latest  .
-[[ -f .docker_push ]] && docker push cr.selcloud.ru/neurolab/neurolab_source:latest
